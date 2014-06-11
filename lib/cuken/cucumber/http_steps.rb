9b266374f4fb9d7c09913242c884cfc46530720d
Josh Bronson <josh.bronson@gmail.com>
2014-06-11 15:04:24 -0500

    feature: javascript and screenshot http steps

--- lib/cuken/cucumber/http_steps.rb
+++ lib/cuken/cucumber/http_steps.rb
@@ -55,6 +55,14 @@ When /^I attach the file at "(.*)" to "(.*)"$/ do |path, field|
   attach_file(field, path)
 end
 
+When /^I execute the following javascript(?: (?:code|script|snippet))?:$/ do |value|
+  page.execute_script(value)
+end
+
+When /^I save a screenshot/ do
+  page.save_screenshot('test.png')
+end
+
 # Results
 Then /^I should see "(.*)"$/ do |text|
   page.body.to_s.should =~ /#{text}/m
